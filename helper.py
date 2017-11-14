495cbdd9f425437b6e2acae98e6090035c9e07a1
Manh Nguyen <manhnguyenhuu7898@yahoo.com.vn>
2017-11-13 20:40:06 -0500

    Delete helper.py

--- helper.py
+++
@@ -1,9 +0,0 @@
-def update(sql_statement, cursor, db):
-    cursor.execute(sql_statement)
-    db.commit()
-
-
-def query(sql_statement, cursor):
-    cursor.execute(sql_statement)
-    return cursor
-
